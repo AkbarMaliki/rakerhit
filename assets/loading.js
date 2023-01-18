753133549a52c65661a0e24c622d5fe34f0f444b
Taufik Akbar Maliki <taufikakbarmalikitkj@gmail.com>
2023-01-18 13:29:11 +0800

    inital

---
+++ assets/loading.js
@@ -0,0 +1,9 @@
+{
+    "apiKey": "AIzaSyAyGzNX-pObPuncM03rXXze7mz5PjIe_7U",
+    "authDomain": "raker-f72ce.firebaseapp.com",
+    "projectId": "raker-f72ce",
+    "storageBucket": "raker-f72ce.appspot.com",
+    "messagingSenderId": "327643291873",
+    "appId": "1:327643291873:web:49ca7a78fa5f6fa0ded7a0",
+    "measurementId": "G-J0CMJ8P2DM"
+}
